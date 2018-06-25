d1a059cd31627d636248551295c36371a88cde1d
Endrick <Endrick@DESKTOP-CQTE0GH>
2018-06-25 16:35:13 -0300

    Adicionando e Editando Telas

--- JogoDaMemoria/app/src/main/java/up/edu/br/jogodamemoria/MainActivity.java
+++ JogoDaMemoria/app/src/main/java/up/edu/br/jogodamemoria/MainActivity.java
@@ -1,7 +1,10 @@
 package up.edu.br.jogodamemoria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnIniciar = (Button)findViewById(R.id.btnIniciar);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(it);
+            }
+        });
+
+    }
+
+
+
+    public void Ranking(View view) {
+
+        Intent it = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(it);
     }
 }
---
+++ JogoDaMemoria/app/src/main/java/up/edu/br/jogodamemoria/ranking_adapter.java
@@ -0,0 +1,13 @@
+package up.edu.br.jogodamemoria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ranking_adapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ranking_adapter);
+    }
+}
